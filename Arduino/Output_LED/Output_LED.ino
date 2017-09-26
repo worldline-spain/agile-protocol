5f45657fcc32cba47633ea0bee429e14abb85d5e
Manel Batlles <manel.batlles@atos.net>
2017-09-26 10:45:56 +0100

    Arduino code for PIR read and LED write

---
+++ Arduino/Output_LED/Output_LED.ino
@@ -0,0 +1,64 @@
+// http://henrysbench.capnfatz.com/henrys-bench/arduino-sensors-and-input/arduino-hc-sr501-motion-sensor-tutorial/
+// https://github.com/andrewrapp/xbee-arduino
+
+#include <XBee.h>
+
+XBee xbee = XBee();
+ZBRxResponse rx = ZBRxResponse();
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  // XBee
+  Serial.begin(9600);
+  xbee.setSerial(Serial);
+  
+  // LED
+  digitalWrite(LED_BUILTIN, LOW);
+
+  flashLed(100);
+  delay(500);
+  flashLed(1000);
+  delay(500);
+  flashLed(100);
+  delay(500);
+}
+
+void loop() {
+  xbee.readPacket();
+  
+    if (xbee.getResponse().isAvailable()) {
+      // got something
+      if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
+        // got a zb rx packet        
+        // now fill our zb rx class
+        xbee.getResponse().getZBRxResponse(rx);
+        
+        // data should be 01-LED-ON or 01-LED-OFF
+        
+        uint8_t* payloadData = rx.getData();
+        uint8_t payloadDataLength = rx.getDataLength();
+        if (payloadDataLength >= 9 && payloadData[7] == 'O' && payloadData[8] == 'N') {
+          digitalWrite(LED_BUILTIN, HIGH);
+        } else if (payloadDataLength >= 10 && payloadData[7] == 'O' && payloadData[8] == 'F' && payloadData[9] == 'F') {
+          digitalWrite(LED_BUILTIN, LOW);
+        } else {
+          flashLed(100);
+          delay(100);
+          flashLed(100);
+        }
+      } else {
+        // not something we were expecting
+        flashLed(1000);
+      }
+    } else if (xbee.getResponse().isError()) {
+        flashLed(1000);
+    }
+}
+
+void flashLed(int d) {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(d);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
---
+++ Arduino/Sensor_PIR/Sensor_PIR.ino
@@ -0,0 +1,56 @@
+// http://henrysbench.capnfatz.com/henrys-bench/arduino-sensors-and-input/arduino-hc-sr501-motion-sensor-tutorial/
+// https://github.com/andrewrapp/xbee-arduino
+
+#include <XBee.h>
+
+const int ledPin = 13;  // LED on Pin 13 of Arduino
+const int pirPin = 7;   // HC-S501 (Passive infrared sensor)
+
+XBee xbee = XBee();
+uint8_t sensor01_payload[] = { '0','1','-', 'P','I','R','-','O','N','-','0' }; // SensorNum - SensorType - SensorVal - Counter (01-PIR-ON-0)
+int oldPirValue = LOW;  // Old sensor value, to detect changes
+int pktCount = 0;       // Single digit counter 0 .. 9
+
+void setup() {
+  // HC-SR501 Motion Detector
+  pinMode(ledPin, OUTPUT);
+  pinMode(pirPin, INPUT);
+
+  // XBee
+  Serial.begin(9600);
+  xbee.setSerial(Serial);
+
+  // LED
+  digitalWrite(ledPin, oldPirValue);
+}
+
+void loop() {
+  int tmpPirValue; // Place to store read PIR Value
+
+  tmpPirValue = digitalRead(pirPin);
+  digitalWrite(ledPin, tmpPirValue);
+
+  if (oldPirValue == LOW && tmpPirValue == HIGH) {
+    digitalWrite(ledPin, tmpPirValue);
+    oldPirValue = tmpPirValue;
+    sendIRDetection();
+    delay(5000);
+  } else if (tmpPirValue == LOW) {
+    digitalWrite(ledPin, tmpPirValue);
+    oldPirValue = tmpPirValue;
+  }
+}
+
+void sendIRDetection() {
+  // Specify the address of the remote XBee (this is the SH + SL); use 0 + 0  to address the concentrator
+  // XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x40e7d69c);
+  XBeeAddress64 addr64 = XBeeAddress64(0x00000000, 0x00000000);
+
+  // Create a TX Request
+  sensor01_payload[sizeof(sensor01_payload)-1] = pktCount +  '0';
+  ++pktCount %= 10;
+  ZBTxRequest zbTx = ZBTxRequest(addr64, sensor01_payload, sizeof(sensor01_payload));    
+
+  // Send your request
+  xbee.send(zbTx);
+}
